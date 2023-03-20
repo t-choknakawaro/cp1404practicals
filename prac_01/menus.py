4fc5b271e1c5ea4298ad21200139b16e506b7508
Troy <supersugoitroy7@gmail.com>
2023-03-20 13:34:28 +0800

    Add practical 1 files

---
+++ prac_01/menus.py
@@ -0,0 +1,14 @@
+MENU = '(H)ello\n(G)oodbye\n(Q)uit'
+name = input('Enter name: ')
+print(MENU)
+choice = input('>>> ').upper()
+while choice != 'Q':
+    if choice == 'H':
+        print('Hello', name)
+    elif choice == 'G':
+        print('Goodbye', name)
+    else:
+        print('Invalid choice')
+    print(MENU)
+    choice = input('>>> ').upper()
+print('Finished.')
