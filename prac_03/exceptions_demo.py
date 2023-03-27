1fbfdd1bf58a608f97c72b06242c151750d4deb6
Troy <supersugoitroy7@gmail.com>
2023-03-27 14:45:40 +0800

    Answer questions about try and except

---
+++ prac_03/exceptions_demo.py
@@ -0,0 +1,27 @@
+"""
+CP1404/CP5632 - Practical
+Answer the following questions:
+1. When will a ValueError occur?
+2. When will a ZeroDivisionError occur?
+3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+"""
+
+try:
+    numerator = int(input("Enter the numerator: "))
+    denominator = int(input("Enter the denominator: "))
+    fraction = numerator / denominator
+    print(fraction)
+except ValueError:
+    print("Numerator and denominator must be valid numbers!")
+except ZeroDivisionError:
+    print("Cannot divide by zero!")
+print("Finished.")
+
+# when will a ValueError occur
+# when an input is string
+
+# when will a ZeroDevisionError occur
+# when an input is 0
+
+# could you change the code to avoid the possibility of a ZeroDivisionError
+# no, i can't
