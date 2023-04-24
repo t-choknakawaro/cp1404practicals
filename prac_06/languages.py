e95a40d1b7679f855c068c47706b625ca18189aa
Troy <supersugoitroy7@gmail.com>
2023-04-24 14:11:25 +0800

    Complete prac 5 languages and programming language

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -7,6 +7,7 @@ print(python)
 
 
 def main():
+    """Create and print names of dynamically typed ProgrammingLanguage"""
     python = ProgrammingLanguage('Python', 'Dynamic', True, 1991)
     ruby = ProgrammingLanguage('Ruby', 'Dynamic', True, 1995)
     visual_basic = ProgrammingLanguage('Visual Basic', 'Static', False, 1991)
