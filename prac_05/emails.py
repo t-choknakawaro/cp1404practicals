98821b5ff3eef25a7bb7a5437d3ade6f98e11ffb
Troy <supersugoitroy7@gmail.com>
2023-12-04 17:51:44 +0800

    Complete email exercise from prac 5

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,1 +1,21 @@
-emails = {}
+def main():
+    email_to_name = {}
+    email = input('Email: ')
+    while email != '':
+        name = get_name_from_email(email)
+        check = input(f'Is your name {name}? (Y/n) ').upper()
+        if check != 'Y' and check != '':
+            name = input('Name: ')
+        email_to_name[email] = name
+        email = input('Email: ')
+
+    for email, name in email_to_name.items():
+        print(f'{name} ({email})')
+
+
+def get_name_from_email(email):
+    name = email.split('@')[0].replace('.', ' ').title()
+    return name
+
+
+main()
