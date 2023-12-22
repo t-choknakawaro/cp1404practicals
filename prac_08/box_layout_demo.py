5cd3b9d18de946467fb0e996f61f456e183c58ed
Troy <supersugoitroy7@gmail.com>
2023-12-23 03:06:16 +0800

    Fixing box layout py file from prac 8

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -9,14 +9,12 @@ class BoxLayoutDemo(App):
         return self.root
 
     def handle_greet(self):
-        print("test")
+        print('greet')
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def reset(self):
-        self.root.ids.output_label.text = 'Enter your name'
-
-
-
+        self.root.ids.input_name.text = ' '
+        self.root.ids.output_label.text = ' '
 
 
 BoxLayoutDemo().run()
